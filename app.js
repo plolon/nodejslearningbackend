d300cad0853c42429b752a7b3ce9c457ff0c8d6a
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-04-03 14:18:24 +0200

    Added bodyparser and created form for post method

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('In the middleware!');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next)=>{
+    console.log('default middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">AddProduct</button></form>');
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.use('/product', (req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next)=>{
+    console.log('default middleware!');
+    res.send('<h1>Welcome at home page<h1>');
+});
+app.listen(3000);
