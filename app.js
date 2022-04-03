2e16684f921651b358aab11865e2b88efaeeafc3
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-04-03 19:28:33 +0200

    Created blank 404 page

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const res = require("express/lib/response");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
