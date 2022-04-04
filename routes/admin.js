d5e6109084a89c3f5e98595c2d0c7e5e99c84510
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-04-04 20:07:45 +0200

    Created path util

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next)=>{
     console.log('default middleware!');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/product', (req,res,next)=>{
