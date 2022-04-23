40a32fc62aafd5d67e2e63a19727c40aee08089d
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-04-23 18:04:03 +0200

    Created partial splitted html/ejs files

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found!'});
+    res.status(404).render('404', {pageTitle: 'Page not found!', path: '404'});
 });
 
 app.listen(3000);
