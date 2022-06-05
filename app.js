11d67763390d69d632f3fb8b0cbe63d6099da0f4
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-06-05 11:15:28 +0200

    Created cart functionality

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const errorController = require('./controllers/error');
 
@@ -33,10 +35,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {constraints: true, onDelete: "CASCADE"});
+// Relations
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
 
-sequelize.sync(/*{force: true}*/)
+sequelize.sync()
 .then(result => {
     //console.log(result);
     return User.findByPk(1);
@@ -49,6 +56,9 @@ sequelize.sync(/*{force: true}*/)
 })
 .then(user => {
     //console.log(user);
+    return user.createCart();
+})
+.then(cart => {
     app.listen(3000);
 })
 .catch(err => console.log(err));
--- models/cart-item.js
+++ models/cart-item.js
@@ -1,13 +1,14 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
-const Cart = sequelize.define('cart', {
+const CartItem = sequelize.define('cart_item', {
     id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true
-    }
+    },
+    quantity: Sequelize.INTEGER
 });
 
-module.exports = Cart;
+module.exports = CartItem;
