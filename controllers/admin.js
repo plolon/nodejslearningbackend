6d8364ca0fd2e15bd132a793cf8eba490e1f0384
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-06-08 23:12:16 +0200

    Updated product update methods to nosql

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const mongodb = require('mongodb');
 
 exports.getAddProduct = (req, res, next)=>{
     res.render('admin/edit-product', {
@@ -23,40 +24,36 @@ exports.postAddProduct = (req, res, next) => {
     .catch(err => console.error(err));
 };
 
-// exports.getEditProduct = (req, res, next)=>{
-//   const editMode = req.query.edit;
-//   if(!editMode){
-//     return res.redirect('/');
-//   }
-//   const prodId = req.params.productId;
-//   Product.findByPk(prodId)
-//   .then(product =>{
-//     if(!product){
-//       res.redirect('/');
-//     }
-//     res.render('admin/edit-product', {
-//       pageTitle: 'Edit Product',
-//       path: '/admin/edit-product',
-//       editing: editMode,
-//       product: product
-//     });
-//   })
-//   .catch(err => console.error(err));
-// };
+exports.getEditProduct = (req, res, next)=>{
+  const editMode = req.query.edit;
+  if(!editMode){
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+  .then(product =>{
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  })
+  .catch(err => console.error(err));
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//     Product.findByPk(req.body.productId)
-//     .then(product=>{
-//       product.title=req.body.title;
-//       product.imageUrl=req.body.imageUrl;
-//       product.price=req.body.price;
-//       product.description=req.body.description;
-//       return product.save();
-//     })
-//     .then(res.redirect('/admin/products'))
-//     .catch(err => console.error(err));
-    
-// };
+exports.postEditProduct = (req, res, next) => {
+      const product = new Product(
+      req.body.title,
+      req.body.price,
+      req.body.description,
+      req.body.imageUrl,
+      new mongodb.ObjectId(req.body.productId)
+      );
+      product.save()
+    .then(res.redirect('/admin/products'))
+    .catch(err => console.error(err));
+};
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   Product.destroy({where: {id: req.body.productId}})
@@ -65,14 +62,14 @@ exports.postAddProduct = (req, res, next) => {
     
 // }
 
-// exports.getProducts = (req, res, next) => {
-//     Product.findAll()
-//     .then(products => {
-//       res.render('admin/products', {
-//         prods: products,
-//         pageTitle: 'Admin Products',
-//         path: '/admin/products'
-//       });
-//     })
-//     .catch(err => console.error(err));
-// };
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.error(err));
+};
--- models/product.js
+++ models/product.js
@@ -1,23 +1,28 @@
-const res = require('express/lib/response');
-
 const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
-    .collection('products')
-    .insertOne(this)
+    let dbOp;
+    if(this._id) { // Update
+       dbOp = db.collection('products')
+       .updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this});
+    } else { // Create
+      dbOp = db.collection('products')
+      .insertOne(this);
+    }
+    return dbOp
     .then(result => {
       console.log('Created Product!');
-      res.redirect('admin/products');
     })
     .catch(err => console.error(err));
   }
@@ -32,6 +37,18 @@ class Product {
       return products;
     })
     .catch(err => console.err(err));
+  }
+
+  static findById(id) {
+    const db = getDb();
+    return db.collection('products')
+    .find({_id: new mongodb.ObjectId(id)})
+    .next()
+    .then(product => {
+      console.log(product);
+      return product;
+    })
+    .catch(err => console.error(err));
   }
 }
 
