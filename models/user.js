144ead370c12bf4dcc5ad63e08bea2a1980f4f9b
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-02 01:01:39 +0200

    Added functionality to fetch orders records

--- models/user.js
+++ models/user.js
@@ -78,8 +78,18 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db.collection("orders")
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: this.cart.items,
+          user: {
+            _id: new mongodb.ObjectId(this._id),
+            name: this.name,
+            email: this.email,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
       .then((result) => {
         this.cart = { items: [] };
         return db
@@ -89,6 +99,14 @@ class User {
             { $set: { cart: { items: [] } } }
           );
       });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ 'user._id': new mongodb.ObjectId(this._id) })
+      .toArray();
   }
 
   static findById(userId) {
--- routes/shop.js
+++ routes/shop.js
@@ -9,9 +9,8 @@ router.get('/products', shopController.getProducts );
 router.get('/products/:productId', shopController.getProduct );
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
-// router.get('/checkout', shopController.getCheckout);
  router.post('/cart-delete-item', shopController.postDeleteCartProduct);
-// router.get('/orders', shopController.getOrders);
+router.get('/orders', shopController.getOrders);
 router.post('/create-order', shopController.postOrder);
 
 module.exports = router;
