3639761efc8d04d6dc49942a44b88fddc1f6ecca
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-01 14:10:47 +0200

    Changed functionality to store more than one product

--- models/user.js
+++ models/user.js
@@ -14,17 +14,31 @@ class User {
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex(cp => {
-    //   return cp.id === product.id;
-    // });
-    product.quantity = 1;
-    const updatedCart = { items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }] };
-    const db = getDb();
-    return db.collection('users')
-    .updateOne(
-      {_id: new mongodb.ObjectId(this._id)},
-      {$set: {cart: updatedCart}
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
     });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      updatedCartItems[cartProductIndex].quantity =
+        this.cart.items[cartProductIndex].quantity + 1;
+    } else {
+      updatedCartItems.push({
+        productId: new mongodb.ObjectId(product._id),
+        quantity: 1,
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
