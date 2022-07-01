4022450c2b1003213f0583f6823d173e65d511b1
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-01 11:59:18 +0200

    Added functionality to override cart in user model

--- models/user.js
+++ models/user.js
@@ -1,26 +1,42 @@
-const getDb = require('../util/database').getDb;
-const mongodb = require('mongodb');
+const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
 
 class User {
-  constructor(name, email){
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
   save() {
     const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp.id === product.id;
+    // });
+    product.quantity = 1;
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
     return db.collection('users')
-      .insertOne(this);
+    .updateOne(
+      {_id: new mongodb.ObjectId(this._id)},
+      {$set: {cart: updatedCart}
+    });
   }
 
   static findById(userId) {
     const db = getDb();
-    return db.collection('users')
-    .findOne({_id: new mongodb.ObjectId(userId)})
-    .then(user => {
-      console.log(user);
-      return user;
-    })
-    .catch(err => console.error(err));
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) })
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => console.error(err));
   }
 }
 
