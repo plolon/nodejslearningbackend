1f8f47fdfd0658681714ca15dd46e7f166c76718
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-03 15:25:32 +0200

    add-product POST

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,76 +1,74 @@
-// const Product = require('../models/product');
+const Product = require('../models/product');
 
-// exports.getAddProduct = (req, res, next)=>{
-//     res.render('admin/edit-product', {
-//         pageTitle: 'Add Product',
-//         path: '/admin/add-product',
-//         editing: false
-//     });
-// };
+exports.getAddProduct = (req, res, next)=>{
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
+    });
+};
 
-// exports.postAddProduct = (req, res, next) => {
-//     const product = new Product(
-//       req.body.title,
-//       req.body.price,
-//       req.body.description,
-//       req.body.imageUrl,
-//       null,
-//       req.user._id
-//     );
-//     product.save()
-//     .then(result => {
-//       console.log(result)
-//       res.redirect('/admin/products');
-//     })
-//     .catch(err => console.error(err));
-// };
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product({
+        title: req.body.title,
+        price: req.body.price,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl
+    });
+    product.save()
+    .then(result => {
+      console.log(result)
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.error(err));
+};
 
-// exports.getEditProduct = (req, res, next)=>{
-//   const editMode = req.query.edit;
-//   if(!editMode){
-//     return res.redirect('/');
-//   }
-//   const prodId = req.params.productId;
-//   Product.findById(prodId)
-//   .then(product =>{
-//     res.render('admin/edit-product', {
-//       pageTitle: 'Edit Product',
-//       path: '/admin/edit-product',
-//       editing: editMode,
-//       product: product
-//     });
-//   })
-//   .catch(err => console.error(err));
-// };
+exports.getEditProduct = (req, res, next)=>{
+  const editMode = req.query.edit;
+  if(!editMode){
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+  .then(product =>{
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  })
+  .catch(err => console.error(err));
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//       const product = new Product(
-//       req.body.title,
-//       req.body.price,
-//       req.body.description,
-//       req.body.imageUrl,
-//       req.body.productId
-//       );
-//       product.save()
-//     .then(res.redirect('/admin/products'))
-//     .catch(err => console.error(err));
-// };
+exports.postEditProduct = (req, res, next) => {
+      const product = new Product(
+      req.body.title,
+      req.body.price,
+      req.body.description,
+      req.body.imageUrl,
+      req.body.productId
+      );
+      product.save()
+    .then(res.redirect('/admin/products'))
+    .catch(err => console.error(err));
+};
 
-// exports.postDeleteProduct = (req, res, next) => {
-//   Product.destroy(req.body.productId)
-//   .then(res.redirect('/admin/products'))
-//   .catch(err => console.error(err));
+exports.postDeleteProduct = (req, res, next) => {
+  Product.destroy(req.body.productId)
+  .then(res.redirect('/admin/products'))
+  .catch(err => console.error(err));
     
-// }
+}
 
-// exports.getProducts = (req, res, next) => {
-//     Product.fetchAll()
-//     .then(products => {
-//       res.render('admin/products', {
-//         prods: products,
-//         pageTitle: 'Admin Products',
-//         path: '/admin/products'
-//       });
-//     })
-//     .catch(err => console.error(err));
-// };
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.error(err));
+};
