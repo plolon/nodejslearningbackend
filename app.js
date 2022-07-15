787719627df0b204a50e2b4ba2c92a5479c308af
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-15 13:29:02 +0200

    Fixed links

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const path = require('path');
-
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config({ path: './credentials.env' });
+
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@node-shop.zriwiyl.mongodb.net/`;
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI =
-  'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop';
-
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'sessions'
+  collection: 'session',
 });
 
 app.set('view engine', 'ejs');
@@ -29,48 +28,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
-    secret: 'my secret',
+    secret: 'dev secret',
     resave: false,
     saveUninitialized: false,
-    store: store
+    store: store,
   })
 );
 
 app.use((req, res, next) => {
-  if (!req.session.user) {
+  if (req.session.isLoggedIn !== true) {
     return next();
   }
-  User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+    User.findById(req.session.user._id)
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((err) => console.error(err));
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-
 app.use(errorController.get404);
 
 mongoose
   .connect(MONGODB_URI)
-  .then(result => {
-    User.findOne().then(user => {
+  .then((result) => {
+    User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
+          name: 'Bob',
+          email: 'bob@email.com',
+          cart: { items: [] },
         });
         user.save();
       }
     });
     app.listen(3000);
   })
-  .catch(err => {
-    console.log(err);
-  });
+  .catch((err) => console.error(err));
