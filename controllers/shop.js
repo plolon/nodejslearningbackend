194e4387848c72eef87fb565f952d0ea0cd3f156
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-18 17:25:27 +0200

    Fixed error with populate

--- controllers/shop.js
+++ controllers/shop.js
@@ -48,7 +48,6 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
-    .execPopulate()
     .then(user => {
       const products = user.cart.items;
       res.render('shop/cart', {
@@ -86,7 +85,6 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
-    .execPopulate()
     .then(user => {
       const products = user.cart.items.map(i => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
