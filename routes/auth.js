5879706e66ef89fc53c74f7108e3fc9f0c69d8c0
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-07-24 12:16:34 +0200

    Created signup fields validation with express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const authController = require('../controllers/auth');
-const { check } = require('express-validator');
+const User = require('../models/user');
+const { body } = require('express-validator');
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.'), authController.postSignup);
+router.post(
+  '/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (user) {
+            return Promise.reject(
+              'E-mail exists already, please pick a different one.'
+            );
+          }
+        });
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 6 characters.'
+    )
+      .isLength({ min: 6 })
+      .isAlphanumeric(),
+    body('confirmPassword', 'Passwords must be same.').custom(
+      (value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match.');
+        }
+        return true;
+      }
+    ),
+  ],
+  authController.postSignup
+);
 router.post('/logout', authController.postLogout);
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
